788819fb8892235e20569686a9e94c572028c9b7
Akshad Pardeshi <ap@rottengrapes.tech>
2025-06-18 16:08:36 +0530

    celery

--- weather/weather/celery.py
+++ weather/weather/celery.py
@@ -2,7 +2,7 @@
 from __future__ import absolute_import, unicode_literals
 import os
 from celery import Celery
-import settings
+from django.conf import settings
 
 # Set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'weather.settings')
